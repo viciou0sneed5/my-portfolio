26054c41d423afa5f538e773360bb98c54ec0bf0
viciou0sneed5 <90607188+viciou0sneed5@users.noreply.github.com>
2025-07-15 02:37:08 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
